f84d9f42430c9fe357ccf5367502acc846d2ec82
manikanta <manikanta3517@gmail.com>
2020-11-30 19:18:53 -0500

    adding element using DOM API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// using Dom API  add html element
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// create element
+let element = document.createElement("h1");
+//set attributes
+element.setAttribute("someRef");
+// add some content.
+element.createTextNode("HaHaHaHaaaaaaaaaaaaaaaaaaaaaaaaaa Keep Smiling ");
+// Now we need to append this h1 node to root node.
+//get root and append
+const rootElement  = document.getElementById("root");
+rootElement.append(element);
